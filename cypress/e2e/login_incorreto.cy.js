5964f733b0778c0f88eea3c99b00e8fb4950f4ca
Douglas Liandi <douglasliandi@hotmail.com>
2024-10-06 19:27:54 +0200

    Novos testes

--- cypress/e2e/login_incorreto.cy.js
+++ cypress/e2e/login_incorreto.cy.js
@@ -5,6 +5,9 @@ describe('Login no site Adopet', ()=>{
     beforeEach(()=>{
         cy.visit('https://adopet-frontend-cypress.vercel.app');
         cy.get('[data-test="login-button"]').click();
+        cy.intercept('POST','https://adopet-api-i8qu.onrender.com/adotante/login',{
+            statusCode:400,
+        }).as('stubPost')
     })
 
 it('Deve preencher os campos do login incorretamente e exibir mensagens de erro especificas',()=>{
@@ -12,6 +15,13 @@ it('Deve preencher os campos do login incorretamente e exibir mensagens de erro 
     cy.cadastro_incorreto('michelfoucaultipa.com', '12deoliveira44') 
     
     })
+
+it('Deve falhar mesmo que os campos sejam preenchidos corretament',()=>{
+    cy.login_correto('michelfoucalt@ipa.com', '12deOliveira4')
+    cy.wait('@stubPost')
+    cy.contains('Falha no login. Consulte suas credenciais.').should('be.visible')
+
+})
 
 })
 
